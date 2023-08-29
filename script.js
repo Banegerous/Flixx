93df649ed04952f7522f7e99403891f1482bb3c0
Banegerous <139186019+Banegerous@users.noreply.github.com>
2023-08-29 12:24:37 -0500

    script.js

--- script.js
+++ script.js
@@ -469,21 +469,21 @@ function addCommas(number) {
 function init() {
     switch (global.currentPage) {
         case '/':
-        case '/index.html':
+        case 'index.html':
             displaySlider();
             initSwiper();
             displayPopularMovies();
             break;
-        case '/shows.html':
+        case 'shows.html':
             displayPopularShows();
             break;
-        case '/movie-details.html':
+        case 'movie-details.html':
             displayMovieDetails();
             break;
-        case '/tv-details.html':
+        case 'tv-details.html':
             displayShowDetails();
             break;
-        case '/search.html':
+        case 'search.html':
             search();
             break;
     }
